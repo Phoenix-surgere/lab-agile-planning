79680524e67507437e46a28283771fd612046d54
Constantine <blackphoenix343@gmail.com>
2023-01-17 00:38:56 +0200

    Added features

--- test_github.py
+++ test_github.py
@@ -1,7 +1,13 @@
 def add(x,y):
-	return x+y
+	z = x+y
+	return z if z>0 else -z
 
 def rem(x,y):
-	return x-y
+	return x-y 
 
-print(f"This is {add(4,3)}")
+
+def addAdv(x,y,z):
+	return x+y+z
+print(f"This is {add(-4,3)}!")
+
+	
